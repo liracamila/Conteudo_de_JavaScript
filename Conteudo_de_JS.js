b86cd265159cc6f84269fdb81bbbfc1e9f012112
liracamila <camilarichellelira@gmail.com>
2023-04-02 17:42:24 -0300

    segundo commit

---
+++ Conteudo_de_JS.js
@@ -0,0 +1,48 @@
+/*var h = 5;
+var b = 2;
+
+area = b * h;
+console.log(area);
+var area;
+*/
+/*
+let h = 4;
+let b = 3;
+let area;
+
+area = b * h;
+console.log(area);
+*/
+/*
+var a = 5;
+var b = 3;
+
+resultado = a + b;
+console.log(resultado);
+var resultado;
+*/
+/*
+let a = 5;
+let b = 3;
+
+let resultado;
+
+resultado = a * b;
+console.log(resultado);
+*/
+/*
+const idadeMinima  = 16;
+ const idadeEleitor = 18;
+
+ if (idadeEleitor > idadeMinima) {  // true
+    console.log("Pode votar");
+ } else {                           // false
+    console.log("Não pode votar");
+ }
+ */
+/*
+ const idadeMinima  = 16;
+ const idadeEleitor = 15;
+                                          // true        // false
+ console.log(idadeEleitor > idadeMinima ? "Pode votar" : "Não pode votar");
+ */
